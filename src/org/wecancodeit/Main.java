a29ccab280fa4bbc6969173f41155a24e04ef9bd
fpemba <fatuma.pemba@yahoo.com>
2020-10-04 11:16:32 -0400

    added player variables

--- src/org/wecancodeit/Main.java
+++ src/org/wecancodeit/Main.java
@@ -13,5 +13,13 @@ public class Main {
         String[] enemies = {"Skeleton","Zombie","Warrior","Assassin"};
         int maxEnemyHealth = 75;
         int enemyAttackDamage = 25;
+
+        int health = 100;
+        int attackDamage = 50;
+        int numHealthPotions = 3;
+        int healthPotionHealthAmount = 30;
+        int healthPotionDropChance = 50;
+
+
     }
 }
