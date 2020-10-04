af250a66a8c26f5d9974b68438b6b8443de3dd69
fpemba <fatuma.pemba@yahoo.com>
2020-10-04 11:11:44 -0400

    added enemy variables

---
+++ src/org/wecancodeit/Main.java
@@ -0,0 +1,17 @@
+package org.wecancodeit;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+
+        String[] enemies = {"Skeleton","Zombie","Warrior","Assassin"};
+        int maxEnemyHealth = 75;
+        int enemyAttackDamage = 25;
+    }
+}
